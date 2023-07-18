1d772e7cbcabe64d62c530745a01fbf53878b17f
RammRomm <113111550+RammRomm@users.noreply.github.com>
2023-07-18 12:10:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
 gsap.to(".FB1", {
   scrollTrigger: ".FB1",
   x: 1000,
   rotation: 360,
   duration: 2,
 });
+
+
+
+gsap.from(".IB1", { duration: 2, x: "-100vw" });
